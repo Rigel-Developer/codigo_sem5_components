d4650e31b48ace2f3c79b89e1b6464cb72285481
jhonny <jhonny.anovo@gmail.com>
2023-08-01 10:20:57 -0500

    Avance hasta video 9 semana 6

--- lib/pages/alert.page.dart
+++ lib/pages/alert.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
   const AlertPage({super.key});
@@ -33,6 +34,104 @@ class AlertPage extends StatelessWidget {
     );
   }
 
+  showAlert2(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          // contentPadding: EdgeInsets.all(10.0),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Soy una alerta",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              const Divider(
+                thickness: 0.7,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const CircleAvatar(
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                  "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle.jpg",
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                "Hola este es el container de la alerta",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.black54,
+                  fontSize: 13,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Cancelar",
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Aceptar",
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +145,14 @@ class AlertPage extends StatelessWidget {
             ElevatedButton(
               onPressed: () {
                 showAlert(context);
+              },
+              child: Text(
+                "Mostrar alerta",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showAlert2(context);
               },
               child: Text(
                 "Mostrar alerta",
